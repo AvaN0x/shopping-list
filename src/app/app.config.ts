4230ebd1a44bf4edfed646fdb0b36d749b508ce2
AvaN0x <clement.ricatte57@gmail.com>
2024-08-23 18:39:37 +0200

    refactor: try to remove ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,14 +6,12 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
